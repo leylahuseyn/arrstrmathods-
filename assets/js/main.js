ed943591c3a248c444effaa9924f58d83b9642ed
leylahuseyn <https://github.com/leylahuseyn/leylahuseyn.git>
2023-04-26 20:35:23 +0400

    initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+let text = prompt("yazi daxil edin!");
+let count=0;
+for (let i = 0; i < text.length; i++) {
+    if (text.includes("a")){
+        text.includes=count++;
+    alert(count);
+}
+else{
+    alert("yanlish yazi");
+}
+}
